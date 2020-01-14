de697d983e6c7cde989e7a577345ff12c8fecb88
Hassan Jamila <hassan.jamila@n2nconnect.com>
2020-01-14 20:27:59 +0800

    Add a label, and a button using code. Add properties and actions and attach them to the component. Test the code and it's working

--- OutletsAndActions/ViewController.swift
+++ OutletsAndActions/ViewController.swift
@@ -9,10 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var count = 0
+    var label:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       let label = UILabel()
+        //Label
+        label.frame =  CGRect(x: 150, y: 150, width: 60, height: 60)
+        label.text = "0"
+        view.addSubview(label)
+        self.label = label
+        //Button
+        let button = UIButton()
+        button.frame = CGRect(x: 120, y: 200, width: 80, height: 30)
+        button.setTitle("Click", for: .normal)
+        button.setTitleColor(UIColor.red, for: .normal)
+        view.addSubview(button)
+        button.addTarget(self, action: #selector(ViewController.onClick), for: .touchUpInside)
+    }
+    
+    @objc func onClick(){
+        self.count += 1
+        self.label.text = "\(self.count)"
     }
 
 
