9c957a8112e6f28a9f65cac5568d1dcc823c59d7
Hassan Jamila <hassan.jamila@n2nconnect.com>
2020-01-14 20:35:15 +0800

    Add another label and button for decrement. Test the code and it's working.

--- OutletsAndActions/ViewController.swift
+++ OutletsAndActions/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
     var count = 0
     var label:UILabel!
     
+    var count1 = 0
+    var decrementLabel:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        let label = UILabel()
@@ -27,11 +30,36 @@ class ViewController: UIViewController {
         button.setTitleColor(UIColor.red, for: .normal)
         view.addSubview(button)
         button.addTarget(self, action: #selector(ViewController.onClick), for: .touchUpInside)
+        
+        
+        //Mark: Add the decrement label
+        let decLabel = UILabel()
+        decLabel.frame = CGRect(x: 120 , y: 250, width: 60, height: 60)
+        decLabel.text = "0"
+        view.addSubview(decLabel)
+        self.decrementLabel = decLabel
+        
+        
+        //Mark: Add decrement button
+        let decButton = UIButton()
+        decButton.frame = CGRect(x: 120, y: 300, width: 100, height: 30)
+        decButton.setTitle("Decrement", for: .normal)
+        decButton.setTitleColor(UIColor.blue, for: .normal)
+        view.addSubview(decButton)
+        
+        decButton.addTarget(self, action: #selector(ViewController.onDecrement), for: .touchUpInside)
+        
+        
     }
     
     @objc func onClick(){
         self.count += 1
         self.label.text = "\(self.count)"
+    }
+    
+    @objc func onDecrement(){
+        count1 -= 1
+        self.decrementLabel.text = "\(count1)"
     }
 
 
